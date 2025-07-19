d61f4addf4645119585a35eac9fca368699ce022
Dashi <dorzhiev_dd@mail.ru>
2025-07-20 00:35:58 +1200

    lesson 50. Создали первый работающий маршрут

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+def landing(request): 
+    return HttpResponse("<h1>Главная страница</h1>")
 
 # Create your views here.
