7e982cce1620c4e7164a03d502eb30a31a4f5624
Dashi <dorzhiev_dd@mail.ru>
2025-07-20 15:00:28 +1200

    lesson 59. Создание маршрутов из ДЗ, представлений из ДЗ, добавление набора данных из ДЗ

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -33,7 +33,7 @@ from core.views import landing, thanks, orders_list, order_detail
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", landing),
-    path("thanks", thanks),
-    path("orders", orders_list),
+    path("thanks/", thanks),
+    path("orders/", orders_list),
     path("orders/<int:order_id>/", order_detail),
 ]
--- core/views.py
+++ core/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, HttpResponse
 
-def landing(request): 
+
+def landing(request):
     return HttpResponse("<h1>Главная страница</h1>")
+
 
 def thanks(request):
     return HttpResponse("<h1>Спасибо за заказ!</h1>")
 
+
 def orders_list(request):
     return HttpResponse("<h1>Список заказов</h1>")
 
-def order_detail(request):
-    return HttpResponse("<h1>Детали заказа</h1>")
+
+def order_detail(request, order_id):
+    return HttpResponse(f"<h1>Детали заказа {order_id}</h1>")
+
 
 # Create your views here.
