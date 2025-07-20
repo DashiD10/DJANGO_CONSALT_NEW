e4e1ed6efd15a3794628ddb557b15155527dc477
Dashi <dorzhiev_dd@mail.ru>
2025-07-20 23:28:09 +1200

    lesson 51. Новый путь, где лежат шаблоны. Шаблон спасибо за заявку. Вью которая рендерит шаблон

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .data import orders
 
 
 def landing(request):
@@ -6,15 +7,29 @@ def landing(request):
 
 
 def thanks(request):
-    return HttpResponse("<h1>Спасибо за заказ!</h1>")
+    return render(request, "thanks.html")
 
 
 def orders_list(request):
     return HttpResponse("<h1>Список заказов</h1>")
 
 
-def order_detail(request, order_id):
-    return HttpResponse(f"<h1>Детали заказа {order_id}</h1>")
 
+def order_detail(request, order_id):
+    """
+    Отвечает за маршрут 'orders/<int:order_id>/'
+    :param request: HttpRequest
+    :param order_id: int (номер заказа)
+    """
+    order = [order for order in orders if order["id"] == order_id]
+    try:
+        order = order[0]
+
+    except IndexError:
+        return HttpResponse("<h1>Заказ не найден</h1>", status=404)
+
+    else:
+        order_data = f"""<h1>Заказ {order["id"]}</h1><p>Клиент: {order["client_name"]}</p>"""
+        return HttpResponse(order_data)
 
 # Create your views here.
