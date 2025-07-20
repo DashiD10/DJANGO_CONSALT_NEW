68bae1ed2ef17185606b032083a2059be993fffa
Dashi <dorzhiev_dd@mail.ru>
2025-07-20 13:50:40 +1200

    lesson 51. Добавили маршруты в urls.py

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -14,11 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+### Таблица маршрутов, представлений и шаблонов
+
+"""
+| Маршрут                     | Представление        | Шаблон                     | Псевдоним маршрута |
+| --------------------------- | -------------------- | -------------------------- | ------------------ |
+| `'/'`                       | `views.landing`      | `./landing.html`           | `landing`          |
+| `'/thanks/'`                | `views.thanks`       | `./core/thanks.html`       | `thanks`           |
+| `'/orders/'`                | `views.orders_list`  | `./core/orders_list.html`  | `orders_list`      |
+| `'/orders/<int:order_id>/'` | `views.order_detail` | `./core/order_detail.html` | `order_detail`     |
+"""
+
 from django.contrib import admin
 from django.urls import path
-from core.views import landing
+from core.views import landing, thanks, orders_list, order_detail
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", landing),
+    path("thanks", thanks),
+    path("orders", orders_list),
+    path("orders/<int:order_id>/", order_detail),
 ]
