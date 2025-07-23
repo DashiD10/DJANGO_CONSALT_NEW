ba241515f60569cf34f6fca2700126772c665221
Dashi <dorzhiev_dd@mail.ru>
2025-07-23 22:28:59 +1200

    lesson 52. Начало экспериментов с типами данных

--- core/views.py
+++ core/views.py
@@ -14,7 +14,6 @@ def orders_list(request):
     return HttpResponse("<h1>Список заказов</h1>")
 
 
-
 def order_detail(request, order_id):
     """
     Отвечает за маршрут 'orders/<int:order_id>/'
@@ -35,9 +34,26 @@ def order_detail(request, order_id):
     else:
         return render(request, "order_detail.html", context=context)
 
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_my_name(self):
+        return f"Меня зовут {self.name}"
+
+    def __str__(self):
+        return f"Это метод __str__: {self.name}"
+
+
+test_list = ["Алевтина", "Бородач", "Гендальф Серый", "Лысый из Игры Престолов"]
+test_dict = {"master": "Алевтина", "age": 25, "is_master": True}
+test_person = Person("Лысый из Игры Престолов", 50)
+
+
 def test_template(request):
     context_data = {
         "variable_1": "Значение переменной 1",
     }
     return render(request, "test_template.html", context=context_data)
-
