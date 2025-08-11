5a632c23d79a50fe81fb8a66ab99af0c150e7f31
Dashi <dorzhiev_dd@mail.ru>
2025-08-11 17:46:24 +1200

    lesson_55 M2M запросы и модель Service

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Master, Order, Service
+
+admin.site.register(Master)
+admin.site.register(Order)
+admin.site.register(Service)
 
 # Register your models here.
