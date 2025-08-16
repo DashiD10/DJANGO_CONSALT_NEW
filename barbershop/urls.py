402ce2a27070adee9d65d0abb6494e9bee903616
Dashi <dorzhiev_dd@mail.ru>
2025-08-16 16:52:12 +1200

    Обновление визульаной части сайта

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,48 +1,36 @@
-"""
-URL configuration for barbershop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-### Таблица маршрутов, представлений и шаблонов
-
-"""
-| Маршрут                     | Представление        | Шаблон                     | Псевдоним маршрута |
-| --------------------------- | -------------------- | -------------------------- | ------------------ |
-| `'/'`                       | `views.landing`      | `./landing.html`           | `landing`          |
-| `'/thanks/'`                | `views.thanks`       | `./core/thanks.html`       | `thanks`           |
-| `'/orders/'`                | `views.orders_list`  | `./core/orders_list.html`  | `orders_list`      |
-| `'/orders/<int:order_id>/'` | `views.order_detail` | `./core/order_detail.html` | `order_detail`     |
-"""
-
 # barbershop/urls.py
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from core.views import landing, thanks, orders_list, order_detail
+from debug_toolbar.toolbar import debug_toolbar_urls
+from core.views import (
+    landing,
+    thanks,
+    orders_list,
+    order_detail,
+    order_create,
+    services_list,
+    order_page,
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", landing, name="main"),
+    path("", landing, name="landing"),
     path("thanks/", thanks, name="thanks"),
     path("orders/", orders_list, name="orders"),
     path("orders/<int:order_id>/", order_detail, name="order_detail"),
-    ]
+    path("order/create/", order_create, name="order-create"),
+    path("services/", services_list, name="services-list"),
+    path("order/", order_page, name="order-page"),
+]
 
 # Добавляем Статику и Медиа ЕСЛИ в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0]
+    )
+    # Подключим Django Debug  Toolbar
+    urlpatterns += debug_toolbar_urls()
